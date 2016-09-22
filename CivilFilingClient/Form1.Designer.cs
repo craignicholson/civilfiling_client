635f7247f383d32d9679b18c47b64b19bacf3472
Craig Nicholson <craig.nicholson@gmail.com>
2016-09-22 17:10:39 -0500

    initial commit

--- CivilFilingClient/Form1.Designer.cs
+++ CivilFilingClient/Form1.Designer.cs
@@ -28,12 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.btnAttach = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAttach
+            // 
+            this.btnAttach.Location = new System.Drawing.Point(802, 91);
+            this.btnAttach.Name = "btnAttach";
+            this.btnAttach.Size = new System.Drawing.Size(75, 45);
+            this.btnAttach.TabIndex = 0;
+            this.btnAttach.Text = "Attach Files";
+            this.btnAttach.UseVisualStyleBackColor = true;
+            this.btnAttach.Click += new System.EventHandler(this.btnAttach_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(631, 187);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(989, 244);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnAttach);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAttach;
+        private System.Windows.Forms.Button btnSend;
     }
 }
 
--- CivilFilingClient/Form1.cs
+++ CivilFilingClient/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,115 @@ namespace CivilFilingClient
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnAttach_Click(object sender, EventArgs e)
+        {
+            // We will only attach the pdf... but we can have many pdf's
+            // Use Case - we will file one case at a time.
+            // Use Case, One xml file and multiple pdfs? I think this is how it should be.
+            // xml file can have many plantiff and defendants?
+            // 
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            CivilFilingServiceReference.@case caseData = new CivilFilingServiceReference.@case();
+            caseData.caseAction="028";
+            caseData.courtSection = "SCP";
+            caseData.defendantCaption = "test Def caption";
+            caseData.demandAmount = new decimal(1000);
+            caseData.plaintiffCaption ="test plantiff caption";
+            caseData.juryDemand = "N";
+            caseData.lawFirmCaseId = "100";
+            caseData.otherCourtActions = "N";
+            caseData.serviceMethod = "03";
+            caseData.venue = "ATL";
+            caseData.venueOfIncident = "ATL";
+
+            CivilFilingServiceReference.attachment att = new CivilFilingServiceReference.attachment();
+            string filePath = @"C:\Users\Craig Nicholson\Documents\Visual Studio 2015\Projects\CivilFilingClient\CivilFilingClient\some.pdf";
+            //TODO: how large are the files?
+            byte[] bytes = File.ReadAllBytes(filePath);
+
+            att.bytes = bytes;
+            att.contentType = "application/pdf";
+            att.docType = "pdf";
+            att.documentCode = "CMPL";
+            att.documentDescription = "Complaint";
+            att.documentName = "Test";
+            att.extention = "pdf";
+
+            CivilFilingServiceReference.fee fee = new CivilFilingServiceReference.fee();
+            fee.accountNumber = "12345";
+            fee.attorneyClientRefNumber = "12345";
+            fee.attorneyFee = new decimal(1000);
+            fee.feeExempt = "N";
+            fee.paymentType = "CG";
+
+            CivilFilingServiceReference.address pAddress = new CivilFilingServiceReference.address();
+            pAddress.addressLine1 = "25, market street";
+            pAddress.addressLine2 = "Trenton";
+            pAddress.stateCode = "NJ";
+            pAddress.zipCode = "12345";
+
+            CivilFilingServiceReference.party plantiff = new CivilFilingServiceReference.party();
+            plantiff.adaAccommodationInd = "N";
+            plantiff.address = pAddress;
+            plantiff.corporationName = "ABC Corp";
+            plantiff.corporationType = "CO";
+            plantiff.interpreterInd = "N";
+            plantiff.partyAffiliation = "ADM";
+            plantiff.partyDescription = "BUS";
+            plantiff.interpreterInd = "N";
+            plantiff.language = "SPA";
+            plantiff.accommodationType = "a";
+
+            CivilFilingServiceReference.party defendant = new CivilFilingServiceReference.party();
+            defendant.adaAccommodationInd = "N";
+            defendant.address = pAddress;
+            defendant.corporationName = "ABC Corp";
+            defendant.corporationType = "CO";
+            defendant.interpreterInd = "N";
+            defendant.partyAffiliation = "ADM";
+            defendant.partyDescription = "BUS";
+            defendant.accommodationType = "a";
+
+            CivilFilingServiceReference.bulkFilingPacket packet = new CivilFilingServiceReference.bulkFilingPacket();
+            //takes and array of attachment[] we just have attachment
+            CivilFilingServiceReference.attachment[] attachments = new CivilFilingServiceReference.attachment[1];
+            attachments[0] = att;
+            packet.attachmentList = attachments;
+            packet.attorneyId = "000000000";
+            packet.attorneyId = "000000000";
+
+            packet.civilCase = caseData;
+            int numberOfDefendants = 1;
+            CivilFilingServiceReference.party[] defendants = new CivilFilingServiceReference.party[numberOfDefendants];
+            defendants[0] = defendant;
+            packet.defendantList = defendants;
+
+            packet.documentRedactionInd = "Y";
+            packet.fee = fee;
+
+            int numberOfPlantiffs = 1;
+            CivilFilingServiceReference.party[] plantiffs = new CivilFilingServiceReference.party[numberOfPlantiffs];
+            plantiffs[0] = plantiff;
+            packet.plaintiffList = plantiffs;
+
+            CivilFilingServiceReference.civilFilingRequest fillingRequest = new CivilFilingServiceReference.civilFilingRequest();
+            fillingRequest.bulkFilingPacket= packet;
+            
+            // Create the proxy
+            // Set the correct endpoint... this is working differnt than expected... old soap stuff...
+            // Add the security in the header
+            // Send the Request
+            // Wait for the reponse
+            // parse out the responses
+            CivilFilingServiceReference.CivilFilingWSClient proxy = new CivilFilingServiceReference.CivilFilingWSClient();
+            Debug.Print(proxy.Endpoint.ToString());
+            CivilFilingServiceReference.civilFilingResponse fillingReponse =  proxy.submitCivilFiling(fillingRequest);
+
         }
     }
 }
