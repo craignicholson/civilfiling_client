8d700d0b00a5468ac429746be57da156293bd3f2
Craig Nicholson <craig.nicholson@gmail.com>
2016-09-24 22:38:20 -0500

    update the Form with the new controls

--- CivilFilingClient/Form1.Designer.cs
+++ CivilFilingClient/Form1.Designer.cs
@@ -30,13 +30,16 @@
         {
             this.btnAttach = new System.Windows.Forms.Button();
             this.btnSend = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // btnAttach
             // 
-            this.btnAttach.Location = new System.Drawing.Point(802, 91);
+            this.btnAttach.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAttach.Location = new System.Drawing.Point(627, 12);
             this.btnAttach.Name = "btnAttach";
-            this.btnAttach.Size = new System.Drawing.Size(75, 45);
+            this.btnAttach.Size = new System.Drawing.Size(75, 31);
             this.btnAttach.TabIndex = 0;
             this.btnAttach.Text = "Attach Files";
             this.btnAttach.UseVisualStyleBackColor = true;
@@ -44,20 +47,39 @@
             // 
             // btnSend
             // 
-            this.btnSend.Location = new System.Drawing.Point(631, 187);
+            this.btnSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSend.Location = new System.Drawing.Point(627, 65);
             this.btnSend.Name = "btnSend";
-            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.Size = new System.Drawing.Size(75, 31);
             this.btnSend.TabIndex = 1;
             this.btnSend.Text = "Send";
             this.btnSend.UseVisualStyleBackColor = true;
             this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
             // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 121);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(690, 111);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(989, 244);
+            this.ClientSize = new System.Drawing.Size(714, 262);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.btnSend);
             this.Controls.Add(this.btnAttach);
             this.Name = "Form1";
+            this.Text = "Civil Filing Client (New Jersey Courts)";
             this.ResumeLayout(false);
 
         }
@@ -66,6 +88,8 @@
 
         private System.Windows.Forms.Button btnAttach;
         private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
