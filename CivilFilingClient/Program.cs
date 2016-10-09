fada5025cf886d5c4173aa6982ba89f9232d3971
Craig Nicholson <craig.nicholson@gmail.com>
2016-10-08 22:58:52 -0500

    adding feature to support CLI arguments'

--- CivilFilingClient/Program.cs
+++ CivilFilingClient/Program.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using NLog;
+using System;
 using System.Windows.Forms;
 
 namespace CivilFilingClient
@@ -9,14 +7,30 @@ namespace CivilFilingClient
     static class Program
     {
         /// <summary>
+        /// Initialize NLog logger
+        /// </summary>
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            string[] args = Environment.GetCommandLineArgs();
+            if (args.Length > 1)
+            {
+                foreach (string arg in args)
+                {
+                    logger.Info("args: " + arg);
+                }
+            }
+            else
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
         }
     }
 }
