b288f9fa1d585155caa1e7425a2635f76c27ac9a
Craig Nicholson <craig.nicholson@gmail.com>
2016-10-11 15:22:13 -0500

    Add features for CLI processing and testing

--- CivilFilingClient/Program.cs
+++ CivilFilingClient/Program.cs
@@ -1,5 +1,6 @@
 ﻿using NLog;
 using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
 
 namespace CivilFilingClient
@@ -9,7 +10,7 @@ namespace CivilFilingClient
         /// <summary>
         /// Initialize NLog logger
         /// </summary>
-        private static Logger logger = LogManager.GetCurrentClassLogger();
+        private static Logger _logger = LogManager.GetCurrentClassLogger();
 
         /// <summary>
         /// The main entry point for the application.
@@ -17,13 +18,33 @@ namespace CivilFilingClient
         [STAThread]
         static void Main()
         {
+            List<string> responses = new List<string>();
+
             string[] args = Environment.GetCommandLineArgs();
-            if (args.Length > 1)
+            // Required 4 parameters - which means five for winform exe
+            //string username, string password, string endpoint, string xmlFilePath, List<string> responses)
+            if (args.Length == 5)
             {
-                foreach (string arg in args)
+                Console.WriteLine("Beginning of CLI Process");
+                _logger.Info("Beginning of CLI Process");
+                foreach (var arg in args)
                 {
-                    logger.Info("args: " + arg);
+                    _logger.Info("args[] : "  + arg);
                 }
+                //1st arg is trash, it is just the app name.
+                var arg1 = args[0];
+                var username = args[1];
+                var password = args[2];
+                var endpoint = args[3];
+                var xmlfilepath = args[4];
+                FileSuitEngine suit = new FileSuitEngine(username, password, endpoint, xmlfilepath, responses);
+                suit.FileSuit();
+                foreach(var log in responses)
+                {
+                    Console.WriteLine(log);
+                }
+                _logger.Info("End of CLI Process");
+                Console.WriteLine("End of CLI Process");
             }
             else
             {
