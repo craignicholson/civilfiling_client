2e20369eb5c8b4bbfaf80152ebdbb7e1746da02d
Craig Nicholson <craig.nicholson@gmail.com>
2016-11-19 13:40:44 -0600

    Way too man changes ... Most of these are just removal of the method overides.

--- CivilFilingClient/Util.cs
+++ CivilFilingClient/Util.cs
@@ -27,63 +27,78 @@ namespace CivilFilingClient
         /// <returns></returns>
         public static CivilFilingServiceReference.bulkFilingPacket ReadXmlfile(string xmlfilepath, List<string> responses, List<CourtCaseFiles> files, out string pdffilepath)
         {
-            CivilFilingServiceReference.bulkFilingPacket bfp = null;
             pdffilepath = string.Empty;
-            responses.Add("Reading file: " + Path.GetFileName(xmlfilepath));
-
-            var xmldoc = new XmlDocument();
-            xmldoc.Load(xmlfilepath);
-            var names = new XmlNamespaceManager(xmldoc.NameTable);
-            names.AddNamespace("xsi", "http://www.w3.org/2001/XMLSchema-instance");
-            names.AddNamespace("soapenv", "http://schemas.xmlsoap.org/soap/envelope/");
-            names.AddNamespace("tns2", "http://webservice.civilfiling.ecourts.ito.aoc.nj/");
-
-            var node = xmldoc.SelectSingleNode("/soapenv:Envelope/soapenv:Body/tns2:submitCivilFiling/arg0/bulkFilingPacket", names);
-            XmlSerializer ser = new XmlSerializer(typeof(CivilFilingServiceReference.bulkFilingPacket));
-
-            // If the xml doc is not formatted with the correct namespaces the node will 
-            // be returned null
-            if (node == null)
+            if (File.Exists(xmlfilepath))
             {
-                var soapmsg = "/soapenv:Envelope/soapenv:Body/tns2:submitCivilFiling/arg0/bulkFilingPacket";
-                var message = "File soap format is incorrect.  Review the namespaces to make sure they contain: " + Environment.NewLine + soapmsg;
-                responses.Add("Error xml nodes should contain: " + message);
-                _logger.Error("Error xml nodes should contain" + message);
+                CivilFilingServiceReference.bulkFilingPacket bfp = null;
+                responses.Add("ReadXmlfile.Deserialize file: " + Path.GetFileName(xmlfilepath));
+                _logger.Info("ReadXmlfile.Deserialize file: " + Path.GetFileName(xmlfilepath));
+
+                var xmldoc = new XmlDocument();
+                xmldoc.Load(xmlfilepath);
+                var names = new XmlNamespaceManager(xmldoc.NameTable);
+                names.AddNamespace("xsi", "http://www.w3.org/2001/XMLSchema-instance");
+                names.AddNamespace("soapenv", "http://schemas.xmlsoap.org/soap/envelope/");
+                names.AddNamespace("tns2", "http://webservice.civilfiling.ecourts.ito.aoc.nj/");
+
+                var node = xmldoc.SelectSingleNode("/soapenv:Envelope/soapenv:Body/tns2:submitCivilFiling/arg0/bulkFilingPacket", names);
+                XmlSerializer ser = new XmlSerializer(typeof(CivilFilingServiceReference.bulkFilingPacket));
+
+                // If the xml doc is not formatted with the correct namespaces the node will 
+                // be returned null
+                if (node == null)
+                {
+                    var soapmsg = "/soapenv:Envelope/soapenv:Body/tns2:submitCivilFiling/arg0/bulkFilingPacket";
+                    var message = "File soap format is incorrect.  Review the namespaces to make sure they contain: " + Environment.NewLine + soapmsg;
+                    responses.Add("Error xml nodes should contain: " + message);
+                    _logger.Error("Error xml nodes should contain" + message);
+                    return null;
+                }
+                try
+                {
+                    bfp = (CivilFilingServiceReference.bulkFilingPacket)ser.Deserialize(new StringReader(node.OuterXml));
+                    string fileName = bfp.attachmentList[0].documentName + bfp.attachmentList[0].extention;
+                    responses.Add("Creating Attachment file: " + fileName);
+
+                    // PDF file might not be in same directory so we should check to 
+                    // to see is we have the PDF in our list of files
+                    CourtCaseFiles pdfFile = files.Find(item => item.FileName.ToUpper() == fileName.ToUpper());
+                    if (pdfFile != null)
+                    {
+                        pdffilepath = pdfFile.FullFilePath;
+                        responses.Add("PDF Exists: " + pdfFile);
+                        _logger.Info("PDF Exists: " + pdfFile);
+
+                    }
+                    else //If users does not attach any file assume the pdf is in same directory as XML file.
+                    {
+                        //TODO: Will a pdf always be a requirement?
+                        responses.Add("PDF not found using root or origin : " + Path.GetDirectoryName(xmlfilepath) + @"\" + fileName);
+                        _logger.Info("PDF not found using root or origin : " + Path.GetDirectoryName(xmlfilepath) + @"\" + fileName);
+
+                        pdffilepath = Path.GetDirectoryName(xmlfilepath) + @"\" + fileName;
+                    }
+                    byte[] bytes = File.ReadAllBytes(pdffilepath);
+                    bfp.attachmentList[0].bytes = bytes;
+                }
+                catch (System.Exception ex)
+                {
+                    var message = "Error reading file : " + ex.Message;
+                    if (ex.InnerException != null)
+                        message = Environment.NewLine + ex.InnerException.Message;
+
+                    responses.Add("Creating Attachment file Error reading file: " + message);
+                    _logger.Error("Creating Attachment file Error reading file: " + message);
+                    // Stop all processing
+                    return null;
+                }
+                return bfp;
+            }
+            else
+            {
+                _logger.Warn("Files does not exist. Check the file path for : " + xmlfilepath);
                 return null;
             }
-            try
-            {
-                bfp = (CivilFilingServiceReference.bulkFilingPacket)ser.Deserialize(new StringReader(node.OuterXml));
-                string fileName = bfp.attachmentList[0].documentName + bfp.attachmentList[0].extention;
-                responses.Add("Creating Attachment file: " + fileName);
-
-                // PDF file might not be in same directory so we should check to 
-                // to see is we have the PDF in our list of files
-                CourtCaseFiles pdfFile = files.Find(item => item.FileName.ToUpper() == fileName.ToUpper());
-                if (pdfFile != null)
-                {
-                    pdffilepath = pdfFile.FullFilePath;
-                }
-                else //If users does not attach any file assume the pdf is in same directory as XML file.
-                {
-                    //TODO: Will a pdf always be a requirement?
-                    pdffilepath = Path.GetDirectoryName(xmlfilepath) + @"\" + fileName;
-                }
-                byte[] bytes = File.ReadAllBytes(pdffilepath);
-                bfp.attachmentList[0].bytes = bytes;
-            }
-            catch (System.Exception ex)
-            {
-                var message = "Error reading file : " + ex.Message;
-                if (ex.InnerException != null)
-                    message = Environment.NewLine + ex.InnerException.Message;
-
-                responses.Add("Error Attaching file: " + message);
-                _logger.Error(message);
-                // Stop all processing
-                return null;
-            }
-            return bfp;
         }
 
         /// <summary>
@@ -99,11 +114,21 @@ namespace CivilFilingClient
             {
                 CivilFilingServiceReference.bulkFilingPacket bfp = null;
                 pdffilepath = string.Empty;
-                responses.Add("Reading file: " + Path.GetFileName(xmlfilepath));
+                responses.Add("ReadXmlfile.Deserialize file: " + Path.GetFileName(xmlfilepath));
                 responses.Add("Attaching file: " + Path.GetFileName(pdffilepath));
+                _logger.Info("ReadXmlfile.Deserialize file: " + Path.GetFileName(xmlfilepath));
+                _logger.Info("ReadXmlfile.Attaching file: " + Path.GetFileName(pdffilepath));
 
                 var xmldoc = new XmlDocument();
-                xmldoc.Load(xmlfilepath);
+                try
+                {
+                    xmldoc.Load(xmlfilepath);  // File IO, needs try catch, but I have .Exits aboveis this too much
+                }
+                catch (System.Exception ex)
+                {
+                    responses.Add("xmldoc.Load file: " + ex.Message + " - " + xmlfilepath);
+                    _logger.Info("xmldoc.Load file: " + ex.Message + " - " + xmlfilepath);
+                }
                 var names = new XmlNamespaceManager(xmldoc.NameTable);
                 names.AddNamespace("xsi", "http://www.w3.org/2001/XMLSchema-instance");
                 names.AddNamespace("soapenv", "http://schemas.xmlsoap.org/soap/envelope/");
@@ -136,8 +161,8 @@ namespace CivilFilingClient
                     if (ex.InnerException != null)
                         message = " | " + ex.InnerException.Message;
 
-                    responses.Add("ReadXmlfile Error reading file: " + message);
-                    _logger.Error("ReadXmlfile Error reading file: " + message);
+                    responses.Add("Creating Attachment file Error reading file: " + message);
+                    _logger.Error("Creating Attachment file Error reading file: " + message);
                     // Stop processing by returning a null
                     return null;
                 }
@@ -164,65 +189,71 @@ namespace CivilFilingClient
         /// <param name="files">List of the CourtCaseFiles we processed</param>
         public static void SaveResponseToFile(List<string> responses, string docketNumber, string xmlFilePath, string pdfFilePath)
         {
-            // Root Folder with xml file, is where we add the folder to archive the files for successful transactions
-            string root_dir = Path.GetDirectoryName(xmlFilePath);
-            // logFilename will have the \ slash on the file so we can skip leaving it on the root_dir
-            string logFilename = string.Empty;
-            string newDirectory = string.Empty;
-
-            // If we had success create the Archive Folder and assign the logFileName as the docketNumber
-            // example : ATL-DC-005347-16_201610061244395579
-            if (docketNumber != "Failed")
-            {
-                newDirectory = DateTime.Now.ToString("yyyyMMdd");
-                Directory.CreateDirectory(root_dir + @"\" + newDirectory);
-                logFilename = @"\" + docketNumber + "_" + DateTime.Now.ToString("yyyyMMddHHmmssffff") + ".txt";
-                responses.Add("Successful Submission Check Log File : " + logFilename);
-                _logger.Info("Successful Submission Check Log File : " + logFilename);
-            }
-            else //File did not load 
-            {
-                // Create Error Log File : Failed_TestCorp2CorpBAD.XML_201610111313522504
-                logFilename = @"\" + Path.GetFileName(xmlFilePath) + "_" + docketNumber + "_" + DateTime.Now.ToString("yyyyMMddHHmmssffff") + ".txt";
-                responses.Add("Failed Submission Check Log File : " + logFilename);
-                _logger.Info("Failed Submission Check Log File : " + logFilename);
-            }
-
             try
             {
-                responses.Add("Writing log file : " + root_dir + logFilename);
-                // All log files are in the root of the origin file dir
-                using (StreamWriter outputFile = new StreamWriter(root_dir + logFilename))
-                {
-                    foreach (string response in responses)
-                        outputFile.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.ffff") + " | " + response);
-                }
-                // When the process is successful
-                // Move the files we processed into the Archive folder (yyyyMMdd)
-                // Otherwise leave the files in the current folder.
+                // Root Folder with xml file, is where we add the folder to archive the files for successful transactions
+                string root_dir = Path.GetDirectoryName(xmlFilePath);
+                // logFilename will have the \ slash on the file so we can skip leaving it on the root_dir
+                string logFilename = string.Empty;
+                string newDirectory = string.Empty;
+                // If we had success create the Archive Folder and assign the logFileName as the docketNumber
+                // example : ATL-DC-005347-16_201610061244395579
                 if (docketNumber != "Failed")
                 {
-                    responses.Add("Archive File : " + Path.GetFileName(xmlFilePath));
-                    responses.Add("Archive File : " + Path.GetFileName(pdfFilePath));
-                    responses.Add("Archive Path : " + root_dir + @"\" + newDirectory);
-                    _logger.Info("Archive File : " + Path.GetFileName(xmlFilePath));
-                    _logger.Info("Archive File : " + Path.GetFileName(pdfFilePath));
-                    _logger.Info("Archive Path : " + root_dir + @"\" + newDirectory);
-                    File.Move(xmlFilePath, root_dir + @"\" + newDirectory + @"\" + Path.GetFileName(xmlFilePath));
-                    File.Move(pdfFilePath, root_dir + @"\" + newDirectory + @"\" + Path.GetFileName(pdfFilePath));
+                    newDirectory = DateTime.Now.ToString("yyyyMMdd");
+                    Directory.CreateDirectory(root_dir + @"\" + newDirectory);
+                    logFilename = @"\" + docketNumber + "_" + DateTime.Now.ToString("yyyyMMddHHmmssffff") + ".txt";
+                    responses.Add("Successful Submission Check Log File : " + logFilename);
+                    _logger.Info("Successful Submission Check Log File : " + logFilename);
                 }
-            }
-            catch (IOException ex)
-            {
-                responses.Add("SaveResponseToFile IOException writing log file : " + ex.Message);
-                _logger.Error(ex.Message);
-                throw;
+                else //File did not load 
+                {
+                    // Create Error Log File : Failed_TestCorp2CorpBAD.XML_201610111313522504
+                    logFilename = @"\" + Path.GetFileName(xmlFilePath) + "_" + docketNumber + "_" + DateTime.Now.ToString("yyyyMMddHHmmssffff") + ".txt";
+                    responses.Add("Failed Submission Check Log File : " + logFilename);
+                    _logger.Info("Failed Submission Check Log File : " + logFilename);
+                }
+                try
+                {
+                    responses.Add("Writing log file : " + root_dir + logFilename);
+                    // All log files are in the root of the origin file dir
+                    using (StreamWriter outputFile = new StreamWriter(root_dir + logFilename))
+                    {
+                        foreach (string response in responses)
+                            outputFile.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.ffff") + " | " + response);
+                    }
+                    // When the process is successful
+                    // Move the files we processed into the Archive folder (yyyyMMdd)
+                    // Otherwise leave the files in the current folder.
+                    if (docketNumber != "Failed")
+                    {
+                        responses.Add("Archive File : " + Path.GetFileName(xmlFilePath));
+                        responses.Add("Archive File : " + Path.GetFileName(pdfFilePath));
+                        responses.Add("Archive Path : " + root_dir + @"\" + newDirectory);
+                        _logger.Info("Archive File : " + Path.GetFileName(xmlFilePath));
+                        _logger.Info("Archive File : " + Path.GetFileName(pdfFilePath));
+                        _logger.Info("Archive Path : " + root_dir + @"\" + newDirectory);
+                        File.Move(xmlFilePath, root_dir + @"\" + newDirectory + @"\" + Path.GetFileName(xmlFilePath));
+                        File.Move(pdfFilePath, root_dir + @"\" + newDirectory + @"\" + Path.GetFileName(pdfFilePath));
+                    }
+                }
+                catch (IOException ex)
+                {
+                    responses.Add("SaveResponseToFile IOException writing log file : " + ex.Message);
+                    _logger.Error(ex.Message);
+                    throw;
+                }
+                catch (System.Exception ex)
+                {
+                    responses.Add("SaveResponseToFile Exception writing log file : " + ex.Message);
+                    _logger.Error(ex.Message);
+                    throw;
+                }
             }
             catch (System.Exception ex)
             {
-                responses.Add("SaveResponseToFile Exception writing log file : " + ex.Message);
-                _logger.Error(ex.Message);
-                throw;
+                responses.Add("Error for " + xmlFilePath + " : " + ex.Message);
+                _logger.Error("Error for " + xmlFilePath + " : " + ex.Message);
             }
         }
     }
